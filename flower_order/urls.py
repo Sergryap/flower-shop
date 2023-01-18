c46431e750bfbd8f7d5cd944ee007ee733270637
sergryap <rs1180@mail.ru>
2023-01-18 23:47:29 +0500

    Some links has been adedd to base.html

--- flower_order/urls.py
+++ flower_order/urls.py
@@ -15,7 +15,7 @@ from .views import (
 app_name = "flower_order"
 
 urlpatterns = [
-    path('', BouquetListView.as_view()),
+    path('', BouquetListView.as_view(), name='start'),
     path('catalog/', CatalogListView.as_view(), name='catalog'),
     path('consultation/', Consultation.as_view(), name='consultation'),
     path('order/', Order.as_view(), name='order'),
