b3d1f46d51dd1cc298eecd3aa365545d2e531335
sergryap <rs1180@mail.ru>
2023-01-18 19:46:47 +0500

    The views.py has been adde

--- flower_order/views.py
+++ flower_order/views.py
@@ -1,39 +1,9 @@
-from django.shortcuts import render
-import json
+from django.views.generic import ListView
+from .models import Bouquet
 
 
-def card(request):
-    return render(request, 'card.html', context={})
-
-
-def catalog(request):
-    return render(request, 'catalog.html', context={})
-
-
-def consultation(request):
-    return render(request, 'consultation.html', context={})
-
-
-def index(request):
-    return render(request, 'index.html', context={})
-
-
-def order_step(request):
-    return render(request, 'order-step.html', context={})
-
-
-def order(request):
-    return render(request, 'order.html', context={})
-
-
-def card(request):
-    return render(request, 'card.html', context={})
-
-
-def quiz(request):
-    return render(request, 'quiz.html', context={})
-
-
-def result(request):
-    return render(request, 'result.html', context={})
+class BouquetListView(ListView):
+    model = Bouquet
+    context_object_name = 'bouquets'
+    template_name = "flower_order/test.html"
 
