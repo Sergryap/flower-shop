24c7750617e82b47a076ad8475a538c76c617f7e
sergryap <rs1180@mail.ru>
2023-01-18 20:56:48 +0500

    The initial views stages have been created

--- flower_order/urls.py
+++ flower_order/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
-from .views import BouquetListView, CatalogListView, Consultation
+from .views import (
+    BouquetListView,
+    CatalogListView,
+    Consultation,
+    Order,
+    OrderStep,
+    Quiz,
+    Result,
+    Card
+)
 
 
 app_name = "flower_order"
@@ -8,5 +17,10 @@ app_name = "flower_order"
 urlpatterns = [
     path('', BouquetListView.as_view()),
     path('catalog/', CatalogListView.as_view()),
-    path('consultation/', Consultation.as_view())
+    path('consultation/', Consultation.as_view()),
+    path('order/', Order.as_view()),
+    path('order-step/', OrderStep.as_view()),
+    path('quiz/', Quiz.as_view()),
+    path('result/', Result.as_view()),
+    path('card/', Card.as_view()),
 ]
--- flower_order/views.py
+++ flower_order/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, View, DetailView
+from django.views.generic import ListView, View, DetailView, TemplateView
 from django.views.generic.detail import TemplateResponseMixin
 from django.views.generic.list import BaseListView, MultipleObjectTemplateResponseMixin, ContextMixin
 from .models import Bouquet
@@ -16,6 +16,25 @@ class CatalogListView(ListView):
     template_name = "flower_order/catalog.html"
 
 
-class Consultation(ListView):
-    model = Bouquet
+class Consultation(TemplateView):
     template_name = "flower_order/consultation.html"
+
+
+class Order(TemplateView):
+    template_name = "flower_order/order.html"
+
+
+class OrderStep(TemplateView):
+    template_name = "flower_order/order-step.html"
+
+
+class Quiz(TemplateView):
+    template_name = "flower_order/quiz.html"
+
+
+class Result(TemplateView):
+    template_name = "flower_order/result.html"
+
+
+class Card(TemplateView):
+    template_name = "flower_order/card.html"
