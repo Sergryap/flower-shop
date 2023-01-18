cc45aecf17a7ac22b5d6f34e59beeabb0754dc25
sergryap <rs1180@mail.ru>
2023-01-18 20:41:38 +0500

    Some views have been added

--- flower_order/views.py
+++ flower_order/views.py
@@ -1,9 +1,21 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, View, DetailView
+from django.views.generic.detail import TemplateResponseMixin
+from django.views.generic.list import BaseListView, MultipleObjectTemplateResponseMixin, ContextMixin
 from .models import Bouquet
 
 
 class BouquetListView(ListView):
     model = Bouquet
     context_object_name = 'bouquets'
-    template_name = "flower_order/test.html"
+    template_name = "flower_order/index.html"
 
+
+class CatalogListView(ListView):
+    model = Bouquet
+    context_object_name = 'bouquets'
+    template_name = "flower_order/catalog.html"
+
+
+class Consultation(ListView):
+    model = Bouquet
+    template_name = "flower_order/consultation.html"
