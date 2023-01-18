f057254be67c091753bc64463be49018938c65d1
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-01-18 21:35:34 +0300

    Updated settings

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -106,22 +106,30 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "flower_order/static"),
+    BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
