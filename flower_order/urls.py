1550be59258c47a50fb8cd46c849c4a41b3a7ba4
Sergryap <rs1180@mail.ru>
2023-01-19 21:07:03 +0500

    The 'catalog/all/' path has been added

--- flower_order/urls.py
+++ flower_order/urls.py
@@ -17,6 +17,7 @@ app_name = "flower_order"
 urlpatterns = [
     path('', BouquetListView.as_view(), name='start'),
     path('catalog/', CatalogListView.as_view(), name='catalog'),
+    path('catalog/all/', CatalogListView.as_view(), name='all_catalog'),
     path('consultation/', Consultation.as_view(), name='consultation'),
     path('order/', Order.as_view(), name='order'),
     path('order-step/', OrderStep.as_view(), name='order_step'),
