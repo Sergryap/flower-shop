cf804a388de656f414b7d6bb907932a680318dc5
Sergryap <rs1180@mail.ru>
2023-01-19 19:20:40 +0500

    Made some changes to templates

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'flower_order' / 'static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
